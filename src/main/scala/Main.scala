8524c46187830463179ce798c4b1aad14f622ea1
Fraser Sprott <Frasers@hotmail.co.uk>
2017-06-05 20:21:45 +0100

    Fixed the logic of the addition and multiplication so no if statements are used

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,37 +9,80 @@ object Main {
     case _ => true
   }
 
-  def add(num1 : Int, num2 : Int) : Int = {
-    var outcome : Int = 0
+  def addition(num1 : Int, num2 : Int) : Int = {
+    var outcome1 : Int = 0
 
-    val pass = notZero(num1)
+    //check number1 if its zero set pass to false
+    var pass : Boolean = notZero(num1)
+    //if pass is true set outcome1 to 1 else 0
     pass match{
-      case true => outcome = 0
-      case false => outcome = 1
+      case true => outcome1 = 1
+      case false => outcome1 = 0
     }
 
-    var pass = notZero(num1)
+    //check number1 if its zero set pass to false
+    pass = notZero(num2)
+    var outcome2 : Int = 0
+    //if pass is true set outcome2 to 2 else 0
     pass match{
-      case true => outcome = 0
-      case false => outcome = 1
+      case true => outcome2 = 2
+      case false => outcome2 = 0
     }
 
-    addValue match{
-      case true
+    //add outcome 1 to outcome 2
+    val outcome3 : Int = outcome1+outcome2
+    //switch outcome3
+    outcome3 match{
+      case 0 => 0 //both were 0 so return o
+      case 1 => num1 //num2 was 0 so return num1
+      case 2 => num2 //num1 was 0 so return num2
+      case 3 => num2+num1 //both had value so add them
     }
   }
 
-  def Calculate(num1 : Int, num2 : Int, add : Boolean) : Int = {
+  def multiplication(num1 : Int, num2 : Int) : Int = {
+    var outcome1 : Int = 0
+
+    //check number1 if its zero set pass to false
+    var pass : Boolean = notZero(num1)
+    //if pass is true set outcome1 to 1 else 0
+    pass match{
+      case true => outcome1 = 1
+      case false => outcome1 = 0
+    }
+
+    //check number1 if its zero set pass to false
+    pass = notZero(num2)
+    var outcome2 : Int = 0
+    //if pass is true set outcome2 to 2 else 0
+    pass match{
+      case true => outcome2 = 2
+      case false => outcome2 = 0
+    }
+
+    //add outcome 1 to outcome 2
+    val outcome3 : Int = outcome1+outcome2
+
+    //switch outcome3
+    outcome3 match{
+      case 0 => 0 //both were 0 so return o
+      case 1 => num1 //num2 was 0 so return num1
+      case 2 => num2 //num1 was 0 so return num2
+      case 3 => num2*num1 //both had value so add them
+    }
+  }
+
+  def calculate(num1 : Int, num2 : Int, add : Boolean) : Int = {
     add match{
-      case true => num1+num2
-      case false => num1*num2
+      case true => addition(num1, num2)
+      case false => multiplication(num1, num2)
     }
   }
 
   def main(args : Array[String]) : Unit = {
-    println("2 + 5 = " + addition(2,5, true))
-    println("2 * 5 = " + addition(2,5,false))
-    //println("0,5 passed: " + addition(0,5,false))
-    //println("2,0 passed: " + addition(2,0,true))
+    println("2 + 5 = " + calculate(2,5, true))
+    println("2 * 5 = " + calculate(2,5,false))
+    println("0,5 passed: " + calculate(0,5,false))
+    println("2,0 passed: " + calculate(2,0,true))
   }
 }
